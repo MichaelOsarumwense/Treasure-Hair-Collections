3eab2c86321d28bff4afe3a9aeaa2be79c86e8cf
MichaelOsarumwense <sanmicad@gmail.com>
2021-02-16 22:03:58 +0000

    Initial commit

---
+++ Treasure_Hair_Collections/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Treasure_Hair_Collections project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Treasure_Hair_Collections.settings')
+
+application = get_asgi_application()
