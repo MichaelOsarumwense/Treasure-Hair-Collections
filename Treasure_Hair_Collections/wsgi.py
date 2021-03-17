c0b4a9d6e3fc43ca0b6cf53258b84edc757b9d4d
MichaelOsarumwense <sanmicad@gmail.com>
2021-03-17 20:28:36 +0000

    Added whitenoise to requirements and formatted wsgi

--- Treasure_Hair_Collections/wsgi.py
+++ Treasure_Hair_Collections/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Treasure_Hair_Collections.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'Treasure_Hair_Collections.settings')
 
 application = get_wsgi_application()
