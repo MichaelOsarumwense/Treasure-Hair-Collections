4cf1d6047741e9b29c5f48812e0bc40ed747333b
MichaelOsarumwense <sanmicad@gmail.com>
2021-03-25 22:24:21 +0000

    Added automated unit test

---
+++ profiles/test_forms.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .forms import UserProfileForm
+
+# Create your tests here.
+
+
+class TestProfileForms(TestCase):
+
+    def test_create_profile_form_with_required_fields_filled(self):
+        form = UserProfileForm({
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County, State or Locality',
+            })
+        self.assertTrue(form.is_valid())
