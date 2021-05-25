7a04b63a76895ee3ecf5fb0c1ad45732c716a2d4
MichaelOsarumwense <sanmicad@gmail.com>
2021-05-25 13:26:15 +0000

    Added view, url and test

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+
+
+c = Client()
+
 
 # Create your tests here.
+class TestSearchViews(TestCase):
+
+    def test_search_redirects_to_all_products_page(self):
+        page = c.get("/search/?q=texas")
+        self.assertEqual(page.status_code, 200)
--- search/views.py
+++ search/views.py
@@ -1,3 +1,23 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from products.models import Product
+from django.db.models import Q
 
-# Create your views here.
+
+def do_search(request):
+    """
+    View for search page. Search results will be displayed in Products page
+    """
+    queries = Q(name__icontains=request.GET['q']) | Q(
+                description__icontains=request.GET['q'])
+    products = Product.objects.filter(queries)
+    paginator = Paginator(products, 4)  # Show 4 products per page
+
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, "products/products.html", {"products": products})
