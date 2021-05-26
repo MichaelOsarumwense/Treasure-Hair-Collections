dbb88f1caa59e73e8269981f5956459071087ec4
sanmicad@gmail.com <sanmicad@gmail.com>
2021-05-26 01:54:28 +0100

    fixed test

--- checkout/test_views.py
+++ checkout/test_views.py
@@ -10,5 +10,3 @@ class CheckoutViews(TestCase):
     def test_checkout_sucess(self):
         response = self.client.get('/checkout_success')
         self.assertTemplateUsed(response, "base.html")
-        self.assertTemplateUsed(response, "includes/mobile-top-header.html")
-        self.assertTemplateUsed(response, "includes/main-nav.html")
--- profiles/test_forms.py
+++ profiles/test_forms.py
@@ -15,5 +15,5 @@ class TestProfileForms(TestCase):
             'default_street_address2': 'Street Address 2',
             'default_county': 'County, State or Locality',
             'default_country': "US",
-            })
+        })
         self.assertTrue(form.is_valid())
